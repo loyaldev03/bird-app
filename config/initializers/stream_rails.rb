c8db14ab153550e547e263f55b6e035ccc772d0b
Victor <ostorodonodor@gmail.com>
2018-03-16 16:23:02 +0700

    Fix

--- config/initializers/stream_rails.rb
+++ config/initializers/stream_rails.rb
@@ -1,8 +1,8 @@
 require 'stream_rails'
 
 StreamRails.configure do |config|
-  config.api_key      = "rsmzjtaqy65g"
-  config.api_secret   = "9nkk9q2wnwvjbtuv6dqkfnew683hrtchkab2rfm84p4k5kmcu7r9jfbxzgkxzd6j"
+  config.api_key      = ENV['STREAM_API_KEY']
+  config.api_secret   = ENV['STREAM_API_SECRET']
   config.timeout      = 30                  # Optional, defaults to 3
   config.location     = 'us-east'           # Optional, defaults to 'us-east'
   # If you use custom feed names, e.g.: timeline_flat, timeline_aggregated,
