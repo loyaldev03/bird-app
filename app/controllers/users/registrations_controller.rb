327b08496630012c7496d6c252b2c7d5f9ba8176
Victor <ostorodonodor@gmail.com>
2018-04-08 02:29:20 +0700

    Fixes

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,6 +3,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-    choose_profile_users_path
+    choose_profile_path
   end
 end
