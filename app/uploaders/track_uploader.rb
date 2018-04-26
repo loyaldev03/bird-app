6867643001b0ac73f17322e84aaa17dc71e5e3c3
Victor <ostorodonodor@gmail.com>
2018-04-26 22:13:04 +0700

    Fix

--- app/uploaders/track_uploader.rb
+++ app/uploaders/track_uploader.rb
@@ -2,7 +2,7 @@ class TrackUploader < CarrierWave::Uploader::Base
   storage :fog
 
   def store_dir
-    key = Digest::MD5.hexdigest model.inspect
+    key = Digest::MD5.hexdigest (model.inspect + mounted_as)
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}/#{key}"
   end
 
