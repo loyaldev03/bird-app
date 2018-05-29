8aeb816fe0d2ba8cbe7966946b8004941a87e8f3
Victor <ostorodonodor@gmail.com>
2018-05-29 18:10:02 +0700

    Fixes

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,8 +3,6 @@ module UsersHelper
   def leaderboard_query page = 1, per_page = 9, all = true
     page = page.to_i
     per_page = per_page.to_i
-    points = "(SELECT SUM(value) FROM badge_points WHERE user_id = users.id)"
-    fans = "LEFT OUTER JOIN users_roles ON (users_roles.user_id = users.id) LEFT OUTER JOIN roles ON (roles.id = users_roles.role_id)"
 
     if all
       @limit = page * per_page
@@ -14,10 +12,27 @@ module UsersHelper
       @offset = (page - 1) * per_page
     end
 
-    sql = "SELECT users.id, users.created_at, #{points} as total FROM users #{fans} WHERE roles.name != 'admin' OR roles.id IS NULL ORDER BY total DESC NULLS LAST, users.created_at ASC LIMIT #{@limit} OFFSET #{@offset}"
-
-    ActiveRecord::Base.connection.execute(sql)
+    users_by_points @limit, @offset
   end
+
+  def user_position user
+    unless user.has_role?(:admin)
+      users_by_points.map{ |u| u['id'] }.index(user.id) + 1
+    end
+  end
+
+  private
+
+    def users_by_points limit=nil, offset=nil
+      points = "(SELECT SUM(value) FROM badge_points WHERE user_id = users.id)"
+
+      fans = "LEFT OUTER JOIN users_roles ON (users_roles.user_id = users.id) LEFT OUTER JOIN roles ON (roles.id = users_roles.role_id)"
+
+      sql = "SELECT users.id, users.created_at, #{points} as total FROM users #{fans} WHERE roles.name != 'admin' OR roles.id IS NULL ORDER BY total DESC NULLS LAST, users.created_at ASC, users.id ASC"
+
+      sql = sql + " LIMIT #{limit}" if limit
+      sql = sql + " OFFSET #{offset}" if offset
+
+      ActiveRecord::Base.connection.execute(sql)
+    end
 end
-
-
