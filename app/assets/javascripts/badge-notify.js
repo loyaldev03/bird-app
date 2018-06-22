dbcbcdcca579a0d678bed0b7ea9372fc16af6f5e
Victor <ostorodonodor@gmail.com>
2018-06-22 23:32:15 +0700

    Badge notify fix

--- app/assets/javascripts/badge-notify.js
+++
@@ -1,8 +0,0 @@
-$(document).on('turbolinks:load', function() {
-  $.get('/badge-notify')
-    .done(function(result){
-      result['badges'].forEach(function(badge){
-        $('#badge' + badge + 'Modal').modal();
-      });
-    });
-});
