e2b7ef981df625c1e52fce8e8bfc03cf5ceb0e57
Victor <ostorodonodor@gmail.com>
2018-06-29 10:59:42 +0700

    Feed badges

--- app/models/badge_level.rb
+++ app/models/badge_level.rb
@@ -1,4 +1,16 @@
 class BadgeLevel < ApplicationRecord
   belongs_to :badge, inverse_of: :badge_levels
   belongs_to :user, inverse_of: :badge_levels
+
+  include StreamRails::Activity
+  as_activity
+
+  def activity_notify
+    [StreamRails.feed_manager.get_feed('user_aggregated', self.user_id)]
+  end
+
+  def activity_object
+    self.badge
+  end
+
 end
