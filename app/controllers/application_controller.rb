f2e12213f23ebe971dd5dff019e367f1048307ba
Victor <ostorodonodor@gmail.com>
2018-09-17 19:54:34 +0700

    player fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -83,7 +83,6 @@ class ApplicationController < ActionController::Base
   protected
     def detect_player_pages
       @player_pages =
-          current_page?(root_path) ||
           (controller?('releases') && action?('index') && params[:player] == true ) ||
           controller?('player') ||
           (controller?('playlists') && action?('show'))
--- app/controllers/topic_categories_controller.rb
+++ app/controllers/topic_categories_controller.rb
@@ -1,5 +1,5 @@
 class TopicCategoriesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:show, :index]
   before_action :set_notifications, only: [:show, :index]
 
   breadcrumb 'Categories', :chirp_index_path, match: :exact
